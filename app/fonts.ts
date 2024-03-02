b1392090645a81fbb9a750c1f8bfbd2ae38f9d68
Rahul Gupta <rg5353070@gmail.com>
2024-03-02 16:57:36 +0530

    added fallback fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -4,9 +4,11 @@ export const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700"],
+  fallback: ["system-ui", "arial"],
 });
 
 export const syne = Syne({
   subsets: ["latin"],
   variable: "--font-syne",
+  fallback: ["system-ui", "arial"],
 });
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-import { Inter, Syne, Poppins } from "next/font/google";
 import { syne, poppins } from "./fonts";
 import "./globals.css";
 import clsx from "clsx";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/theme-provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hero Technologies: Empowering Growth Through Independent Sponsorship",
@@ -21,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${syne.variable} ${poppins.variable}`}>
+    <html lang="en" className={clsx(syne.variable, poppins.variable)}>
       <body className={clsx("min-h-screen bg-background antialiased")}>
         <ThemeProvider
           attribute="class"
